a0a7135a07b25614fd18fd3895128d2683123922
Артём <112126702+Artormvladis@users.noreply.github.com>
2023-03-10 13:37:34 +0300

    Add files via upload

---
+++ funk2.cpp
@@ -0,0 +1,16 @@
+#include "middle_list.h"
+long itc_sum_even_lst(const vector <int> &lst){
+    int a;
+    a=0;
+    for(int i=0; i<lst.size(); i+=2){
+    a+=lst[i];
+    }
+    return a;
+}
+long itc_sum_even_part_lst(const vector <int> &lst){
+    int a;
+    a=0;
+    for(int i=0; i<lst.size(); i++){
+
+    }
+}
