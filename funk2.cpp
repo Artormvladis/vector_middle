8b3be85c2a1fd9a6d3989193879a44d3ed25b54f
Артём <112126702+Artormvladis@users.noreply.github.com>
2023-03-13 14:17:38 +0300

    Update funk2.cpp

--- funk2.cpp
+++ funk2.cpp
@@ -14,3 +14,18 @@ long itc_sum_even_part_lst(const vector <int> &lst){
 
     }
 }
+void itc_odd_even_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2){
+    for(int i=0; i<=lst.size(); i++){
+        if(lst[i]%2==0){
+            lst1.push_back(lst[i]);
+        } else {
+            lst2.push_back(lst[i]);
+        }
+    }
+    for(int i1=0; i<=lst1.size(); i1++){
+        cout<<lst1[i1]<<" ";
+    }
+    for(int i2=0; i<=lst2.size(); i2++){
+        cout<<lst2[i2]<<" ";
+    }
+}
