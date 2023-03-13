dc4c3a0a00fc06f68a426612c6d1c45652a6cb8a
Артём <112126702+Artormvladis@users.noreply.github.com>
2023-03-13 14:25:17 +0300

    Update funk2.cpp

--- funk2.cpp
+++ funk2.cpp
@@ -22,10 +22,30 @@ void itc_odd_even_separator_lst(const vector <int> &lst, vector <int> &lst1, vec
             lst2.push_back(lst[i]);
         }
     }
-    for(int i1=0; i<=lst1.size(); i1++){
+    for(int i1=0; i1<=lst1.size(); i1++){
         cout<<lst1[i1]<<" ";
     }
-    for(int i2=0; i<=lst2.size(); i2++){
+    for(int i2=0; i2<=lst2.size(); i2++){
         cout<<lst2[i2]<<" ";
+    }
+}
+void itc_pos_neg_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2, vector <int> &lst3){
+    for(int i=0; i<=lst.size(); i++){
+        if(lst[i]<0){
+            lst1.push_back(lst[i]);
+        } else if(lst[i]==0){
+            lst2.push_back(lst[i]);
+        } else {
+            lst3.push_back(lst[i]);
+        }
+    }
+    for(int i1=0; i1<=lst1.size(); i1++){
+        cout<<lst1[i1]<<" ";
+    }
+    for(int i2=0; i2<=lst2.size(); i2++){
+        cout<<lst2[i2]<<" ";
+    }
+    for(int i3=0; i3<=lst3.size(); i3++){
+        cout<<lst3[i3]<<" ";
     }
 }
