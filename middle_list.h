24bc5aa8983af526301a0d65f5397028f2a6b616
Артём <112126702+Artormvladis@users.noreply.github.com>
2023-03-13 22:03:16 +0300

    Update middle_list.h

--- middle_list.h
+++ middle_list.h
@@ -12,6 +12,7 @@ long itc_sumlst(const vector <int> &lst);
 long itc_sum_even_lst(const vector <int> &lst);
 long itc_sum_even_part_lst(const vector <int> &lst);
 void itc_odd_even_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2);
+void itc_pos_neg_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2, vector <int> &lst3);
 int itc_max(const vector <int> &mass);
 int itc_min(const vector <int> &mass)
 #endif
